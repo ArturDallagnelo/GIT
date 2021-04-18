b7cc844214fc93eba76c44fd6cabdafc03ab497e
Artur <artur.dall@gmail.com>
2021-04-18 19:34:41 -0300

    Commit pós tag

--- untitled/src/Competicao.java
+++ untitled/src/Competicao.java
@@ -1,2 +1,60 @@
 public class Competicao {
+
+    private String nome;
+    private int qtdTimer;
+    private double premiacao;
+    private int jogos;
+
+    public Competicao() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQtdTimer() {
+        return qtdTimer;
+    }
+
+    public void setQtdTimer(int qtdTimer) {
+        this.qtdTimer = qtdTimer;
+    }
+
+    public double getPremiacao() {
+        return premiacao;
+    }
+
+    public void setPremiacao(double premiacao) {
+        this.premiacao = premiacao;
+    }
+
+    public int getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(int jogos) {
+        this.jogos = jogos;
+    }
+
+    public void Inscricao(){
+
+    }
+
+    public void Chaves(){
+        //switch case - competicao
+    }
+
+    public void Competicao(){
+
+    }
+
+
+
+
+
+
+
 }
